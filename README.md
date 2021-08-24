a2c49b18b40c340d899db22622e46bd6e59e9455
Mozorianer <maurice.hoeft93@gmail.com>
2021-08-24 23:50:04 +0200

    Initial Commit - Changed UniSwap Factory to Dint

---
+++ README.md
@@ -0,0 +1,5 @@
+# DintSwap SDK
+
+In-depth documentation on this SDK is available at [uniswap.org](https://uniswap.org/docs/v2/SDK/getting-started/).
+
+This modifies uniswap-sdk's UniswapV2Factory address. The new address for DintSwap is  `0xEfbc2e209A8cC6C2f46E124DfA8B8E3992c15235`.
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0xEfbc2e209A8cC6C2f46E124DfA8B8E3992c15235'
+
+export const INIT_CODE_HASH = '0xf176ce2ef2ec9c3333f7ab282e4269fdd75024da47415e2c7c6e04272fc1bfab'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
